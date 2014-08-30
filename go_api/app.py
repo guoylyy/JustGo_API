a08df6903f571bf6662221401bcb669595c547b7
guoylyy <guoylyy@gmail.com>
2014-08-30 21:01:34 +0800

    update release

--- go_api/app.py
+++ go_api/app.py
@@ -306,7 +306,7 @@ def make_goal_data():
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host='0.0.0.0',port=5000)
 	#_test_categories()
 	#init_db()
 #================= APIs ========================#
